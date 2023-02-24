664dd26157b398e69237770efa26b05c42c4592c
태고밍 <68893329+TaeKyeong-coder@users.noreply.github.com>
2023-02-24 15:23:06 +0900

    make card

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="TaeTae" backgroundColor="#4ea04e">
+            <p>안녕하세요, 저예요.</p>
+            <p>잠와요</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
